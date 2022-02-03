339b80a59061711e855da515308088cd17759be9
a-baade <andreasbaade@hotmail.com>
2022-02-03 16:24:40 +0100

    test coverage

--- pratice3/__tests__/quizGame.test.jsx
+++ pratice3/__tests__/quizGame.test.jsx
@@ -1,11 +1,26 @@
 import React from "react";
 import ReactDom from "react-dom";
-import {FrontPage, QuizGame, QuestionContext} from "../quizGame";
+import {FrontPage, QuizGame, QuestionContext, ShowQuestion} from "../quizGame";
 import {MemoryRouter} from "react-router-dom";
+import {Simulate} from "react-dom/test-utils";
 import pretty from "pretty";
 
+const question = {
+    question: "How are you?",
+    answers: {
+        answer_a: "Good",
+        answer_b: "Bad",
+        answer_c: "OK",
+    },
+    correct_answers: {
+        answer_a_correct: "true",
+        answer_b_correct: "false",
+        answer_c_correct: "false",
+    }
+}
 
 describe("Quiz game", () => {
+
     it("Shows answer status",() => {
         const element = document.createElement("div");
         ReactDom.render(
@@ -36,4 +51,23 @@ describe("Quiz game", () => {
         );
         expect(pretty(element.innerHTML)).toMatchSnapshot();
     })
+    it("records correct answer", () => {
+
+        const setQuestionsAnswered = jest.fn()
+        const setCorrectAnswers = jest.fn()
+
+        const element = document.createElement("div");
+        ReactDom.render(
+            <MemoryRouter initialEntries={["/question"]}>
+                <QuestionContext.Provider value={{randomQuestion: () => question}}>
+                    <ShowQuestion setCorrectAnswers={setCorrectAnswers} setQuestionsAnswered={setQuestionsAnswered}/>
+                </QuestionContext.Provider>
+            </MemoryRouter>,
+            element
+        );
+        Simulate.click(element.querySelector("[data-testid=answer_a] button"));
+        expect(setQuestionsAnswered).toBeCalled();
+        expect(setCorrectAnswers).toBeCalled();
+        expect(pretty(element.innerHTML)).toMatchSnapshot();
+    });
 })
